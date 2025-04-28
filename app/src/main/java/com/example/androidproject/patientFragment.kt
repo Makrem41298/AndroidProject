68e84dc1df3bc5c5247df5ee1e817e4cd7d2a7f7
Samah <samahbenabdallah793@gmail.com>
2025-04-28 12:00:39 +0100

    updates

--- app/src/main/java/com/example/androidproject/patientFragment.kt
+++ app/src/main/java/com/example/androidproject/patientFragment.kt
@@ -13,7 +13,7 @@ class patientFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
+        return inflater.inflate(R.layout.fragment_patient, container, false)
     }
 
 
